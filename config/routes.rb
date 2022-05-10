eac199018ac81d685cdad33beca71f982909a310
huku29 <sowacham112@gmail.com>
2022-05-10 23:42:31 +0900

    Usersコントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root to: 'static_pages#top'
   get 'static_pages/top'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
