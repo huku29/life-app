d9344295cb7934c40390a24a16092cdffc795f2c
huku29 <sowacham112@gmail.com>
2022-05-13 14:24:56 +0900

    フラッシュメッセージの設定削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,8 @@ class UsersController < ApplicationController
     binding.pry
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path, success: '新規登録完了'
+      redirect_to login_path
     else
-      flash.now[:danger] = '登録失敗'
       render action: :new
     end
   end
