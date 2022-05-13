62c9cb171c9e98e262bdc331973a627ca2e61b28
huku29 <sowacham112@gmail.com>
2022-05-13 12:09:27 +0900

    ログイン、ログアウトのルート指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
+  get 'login', to:'user_sessions#new'
+  post 'login', to:'user_sessions#create'
+  delete 'logout', to:'user_sessions#destroy'
   get 'users/new'
   get 'users/create'
   root to: 'static_pages#top'
   get 'static_pages/top'
-  resources :users
+  resources :users, only: %i[new create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
