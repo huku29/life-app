4c09c2236827c1cbb7a2fcd54362dc712ff2a44b
huku29 <sowacham112@gmail.com>
2022-05-13 12:14:06 +0900

    新規登録画面とUsersコントローラのアクション記載

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+
   def new
+    @user= User.new
   end
 
   def create
+    binding.pry
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path, success: '新規登録完了'
+    else
+      flash.now[:danger] = '登録失敗'
+      render action: :new
+    end
   end
+
+  private 
+  
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
 end
